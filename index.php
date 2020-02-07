159fd73e6aecef1f0e3c7c1f4b8d856e18eb32df
Chandra Shekhar Saw <50990383+Chandrashekhar9@users.noreply.github.com>
2020-02-08 02:08:26 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,41 @@
-<h1>This site is on under process</h1>
+<?php
+
+$to = "ace.cs99@gmail.com";
+
+$subject = "Contact Lead";
+
+$message = "<html>
+<head>
+<title>HTML email</title>
+</head>
+<body>
+<p>This email contains HTML Tags!</p>
+<table>
+<tr>
+<th>Firstname</th>
+<th>Lastname</th>
+</tr>
+<tr>
+<td>John</td>
+<td>Doe</td>
+</tr>
+</table>
+</body>
+</html>";
+
+$header = 'From: <ace.cs99@hotmail.com>' . "\r\n";
+$header .= 'Content-type:text/html;charset=UTF-8' . "\r\n";
+
+
+if(mail($to,$subject,$message,$header)){
+
+echo "Email send successfully";
+
+}else{
+
+echo "Email Failed";
+
+}
+
+
+?>
